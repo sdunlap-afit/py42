2f2b30ad24bbcf46ab8d0fd932e9289ed695488e
sdunlap <sdunlap42@gmail.com>
2024-06-05 20:38:56 +0000

    Plotting functions and test case

--- ecef_plot.py
+++ ecef_plot.py
@@ -9,41 +9,85 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.spatial.transform import Rotation
 
 # 3.429742e+06 -5.846064e+06 6.042774e+04
 
 %matplotlib widget
+plt.rcParams["figure.figsize"] = (8, 8)
 
-def plot_pos(pos_file, title=None, plot_earth=False):
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_title(title)
-    
-    if plot_earth:
-        # Plot earth
-        # https://medium.com/@Romain.p/plot-satellites-real-time-orbits-with-python-s-matplotlib-3c7ccd737638
-        r = 6370000
-        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-        ax.plot_wireframe(r*np.cos(u)*np.sin(v),r*np.sin(u)*np.sin(v),r*np.cos(v),color="g",alpha=0.5,lw=0.5,zorder=0)
 
-    # Read coordinates from space delimited text file
-    data = np.genfromtxt(pos_file, delimiter=' ')
+def plot_earth(ax):
+    # https://medium.com/@Romain.p/plot-satellites-real-time-orbits-with-python-s-matplotlib-3c7ccd737638
+    r = 6370000
+    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    ax.plot_wireframe(r*np.cos(u)*np.sin(v),r*np.sin(u)*np.sin(v),r*np.cos(v),color="g",alpha=0.5,lw=0.5,zorder=0)
+
+
+def plot_pos(ax, pos):
+    '''Plot position'''
 
     # Plot
-    ax.plot(data[:,0], data[:,1], data[:,2])
+    ax.plot(pos[:,0], pos[:,1], pos[:,2])
 
     # Plot first and last point
-    # ax.scatter(data[1,0], data[1,1], data[1,2], c='r')
-    # ax.scatter(data[-1,0], data[-1,1], data[-1,2], c='orange')
+    ax.scatter(pos[1,0], pos[1,1], pos[1,2], c='r')
+    ax.scatter(pos[-1,0], pos[-1,1], pos[-1,2], c='orange')
 
 
-    plt.show()
 
-plot_pos('testsc/PosW.42', title='ECEF Coordinates', plot_earth=True)
-plot_pos('testsc/PosN.42', title='ECI Coordinates' , plot_earth=True)
+def plot_vel(ax, vel, pos):
+    '''Plot velocity vector at position'''
+    
+    # Plot velocity vector at position
+    for i in range(0, len(vel), 10):
+        ax.quiver(pos[i,0], pos[i,1], pos[i,2], vel[i,0], vel[i,1], vel[i,2], length=1e6, normalize=True, color='r')
 
 
+
+def plot_quat(ax, qbn, posN):
+    '''Plot attitude vector at position'''
+    
+    # Plot attitude vector at position
+    for i in range(0, len(qbn), 5):
+        # Rotate vector
+        vs = [  Rotation.from_quat(qbn[i]).apply([1, 0, 0]),
+                Rotation.from_quat(qbn[i]).apply([0, 1, 0]),
+                Rotation.from_quat(qbn[i]).apply([0, 0, 1])]
+
+        for v, c in zip(vs, ['r', 'g', 'b']):
+            ax.quiver(posN[i,0], posN[i,1], posN[i,2], v[0], v[1], v[2], length=0.5e6, normalize=True, color=c)
+
+
+
+DATA_DIR = 'testsc'
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title('ECI Coordinates')
+ax.set_xlabel('X (m)')
+ax.set_ylabel('Y (m)')
+ax.set_zlabel('Z (m)')
+
+# Read coordinates from space delimited text file
+posN = np.genfromtxt(os.path.join(DATA_DIR, 'PosN.42'), delimiter=' ')
+velN = np.genfromtxt(os.path.join(DATA_DIR, 'VelN.42'), delimiter=' ')
+qbn  = np.genfromtxt(os.path.join(DATA_DIR, 'qbn.42' ), delimiter=' ')
+
+plot_earth(ax)
+plot_pos(ax, posN)
+# plot_vel(ax, velN, posN)
+plot_quat(ax, qbn, posN)
+
+
+# Adjust plot limits to be the same
+max_range = np.array([posN.max(), posN.max(), posN.max()]).max()
+ax.set_xlim(-max_range, max_range)
+ax.set_ylim(-max_range, max_range)
+ax.set_zlim(-max_range, max_range)
+
+
+plt.show()
 
 
 # %%
