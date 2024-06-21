f53912813dfff7110dd10bd54e548f6bd050b12f
sdunlap-afit <sdunlap42@gmail.com>
2024-06-21 17:12:43 +0000

    Test runner for kubernetes

--- k8runner.sh
+++ k8runner.sh
@@ -11,27 +11,29 @@
 #   5. Push them back to the share
 
 
-TEST_DIR=$1
-INDEX=$2
+# TEST_DIR=$1
+# INDEX=$2
 
+# Get a random number
+INDEX=$(shuf -i 1-100000 -n 1) && touch run$INDEX.txt && smbclient //pinas/shared -U pi% -c "put run$INDEX.txt"
 
-smbclient //pinas/shared -U pi% -c "get $TEST_DIR.tar.gz"
-smbclient //pinas/shared -U pi% -c "get monte_carlo.py"
+# smbclient //pinas/shared -U pi% -c "get $TEST_DIR.tar.gz"
+# smbclient //pinas/shared -U pi% -c "get monte_carlo.py"
 
-tar -xzf $TEST_DIR.tar.gz
+# tar -xzf $TEST_DIR.tar.gz
 
-cd $TEST_DIR
+# cd $TEST_DIR
 
-python3 monte_carlo.py -t $TEST_DIR -n 1 -c 1 -i $INDEX
+# python3 monte_carlo.py -t $TEST_DIR -n 1 -c 1 -i $INDEX
 
-cd mc_data
+# cd mc_data
 
-# Find the directory created with the results
-DIRNAME = $(ls -d */)
+# # Find the directory created with the results
+# DIRNAME = $(ls -d */)
 
-tar -czf $DIRNAME.tar.gz $DIRNAME
+# tar -czf $DIRNAME.tar.gz $DIRNAME
 
-smbclient //pinas/shared -U pi% -c "put $DIRNAME.tar.gz"
+# smbclient //pinas/shared -U pi% -c "put $DIRNAME.tar.gz"
 
 
 
